6b27750ba34336c235cf5f0f12067e76aee837c0
Sacha Bron <sacha.bron@copperflake.com>
2016-01-22 15:19:46 +0100

    Change data display

--- controller.js
+++ controller.js
@@ -1,14 +1,17 @@
 'use strict';
 
 let bestFanApp = angular.module('bestFanApp', []);
-let username = "BinaryBrain";
+let username = "wasadigi";
+
+let secrets = "?client_id=893e4578ea3d2ae682a6&client_secret=2295a08d3545182af494410a7a516cc36090bc13";
+
 let waitingRequests = 0;
 
 bestFanApp.controller('mainController', function ($scope, $http) {
 	$scope.fans = [];
 	let stargazers = []
 
-	$http.get(`https://api.github.com/users/${username}/repos`).then(response => {
+	$http.get(`https://api.github.com/users/${username}/repos` + secrets).then(response => {
 		let data = response.data;
 		let fanUrls = [];
 		for (let repo of data) {
@@ -16,7 +19,7 @@ bestFanApp.controller('mainController', function ($scope, $http) {
 				fanUrls.push(repo.stargazers_url);
 
 				waitingRequests++;
-				$http.get(repo.stargazers_url).then(response => {
+				$http.get(repo.stargazers_url + secrets).then(response => {
 					for (let user of response.data) {
 						stargazers.push(user);
 					}
@@ -24,29 +27,7 @@ bestFanApp.controller('mainController', function ($scope, $http) {
 					waitingRequests--;
 					
 					if (waitingRequests == 0) {
-						let init = true;
-						let output = stargazers.reduce((prev, curr) => {
-							// Init
-							if (init) {
-								prev.counter = 1;
-								prev = new Map([[prev.login, prev]]);
-								init = false;
-							}
-
-							// Reduce
-							if (prev.has(curr.login)) {
-								prev.get(curr.login).counter++;
-							} else {
-								curr.counter = 1;
-								prev.set(curr.login, curr);
-							}
-
-							return prev;
-						});
-
-						let fans = Array.from(output.values());
-
-						console.info(fans);
+						let fans = countFans(stargazers);
 						$scope.fans = fans;
 					};
 				});
@@ -54,3 +35,43 @@ bestFanApp.controller('mainController', function ($scope, $http) {
 		}
 	});
 });
+
+function countFans(stargazers) {
+	let init = true;
+	if (stargazers.length == 1) {
+		stargazers[0].counter = 1;
+		return stargazers;
+	}
+	
+	let output = stargazers.reduce((prev, curr) => {
+		// Init
+		if (init) {
+			prev.counter = 1;
+			prev = new Map([[prev.login, prev]]);
+			init = false;
+		}
+
+		// Reduce
+		if (prev.has(curr.login)) {
+			prev.get(curr.login).counter++;
+		} else {
+			curr.counter = 1;
+			prev.set(curr.login, curr);
+		}
+
+		return prev;
+	});
+
+	let fans = Array.from(output.values());
+	return fans.sort(compareFans);
+}
+
+function compareFans(a, b) {
+	if (a.counter > b.counter) {
+		return -1;
+	} else if (a.counter < b.counter) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
